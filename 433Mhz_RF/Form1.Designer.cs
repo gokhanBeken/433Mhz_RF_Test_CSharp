37b1a8354412c21a517b6cc5f3fd7bea23272426
gokhanBeken <gokhan60@gmail.com>
2014-09-27 02:57:03 +0300

    İlk versiyon
    Sorunsuz çalışıyor.

---
+++ 433Mhz_RF/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace _433Mhz_RF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(364, 37);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 62);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Port Connection";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(93, 23);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(67, 21);
+            this.comboBox2.TabIndex = 4;
+            this.comboBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBox2_KeyDown);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 80);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(386, 91);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Print";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Location = new System.Drawing.Point(12, 177);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(386, 176);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Data history";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(6, 24);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(374, 117);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 365);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Status:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.radioButton2);
+            this.groupBox4.Controls.Add(this.radioButton1);
+            this.groupBox4.Location = new System.Drawing.Point(287, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(111, 62);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Mode";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(22, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Transmitter";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(22, 39);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(68, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Receiver";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 383);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "433Mhz RF Test | GokhanBeken.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ 433Mhz_RF/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace _433Mhz_RF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        byte[] gelenVeri = new byte[100];
+        int gelenSayac = 0;
+        int durum = 0;
+        bool veriAlmaAktifMi = false;
+
+        void GorunumModeAlici(string mode)
+        {
+            if (mode == "alici")
+            {
+                groupBox2.Text = "Last data";
+                veriAlmaAktifMi = true;
+                button2.Visible = false;
+            }
+            if (mode == "verici")
+            {
+                groupBox2.Text = "Print";
+                veriAlmaAktifMi = false;
+                button2.Visible = true;
+            }
+        }
+
+        bool seri_port_baglan(string port, string baudRate)
+        {
+            try
+            {
+                serialPort1.BaudRate = int.Parse(baudRate);
+                serialPort1.DataBits = int.Parse("8"); // Veri bit ini de 8 bit olarak verdik
+                serialPort1.StopBits = System.IO.Ports.StopBits.One; // Durma bitini tek sefer olarak verdik.
+                serialPort1.Parity = Parity.None; // eşlik bit ini vermedik.
+                serialPort1.PortName = port;
+                serialPort1.Open(); // Bağlantıyı açıyoruz
+
+                return true;
+            }
+
+            catch (Exception) // Herhangi bir hata anında alttaki hata mesajını alacağız..
+            {
+                return false;
+            }
+        }
+
+        public string trDuzelt(string a) //Türkçe karakerleri, ingilizce karakterlere çevirmek için kullanıyoruz
+        {
+            a = a.Replace("İ", "I");
+            a = a.Replace("Ü", "U");
+            a = a.Replace("Ç", "C");
+            a = a.Replace("Ş", "S");
+            a = a.Replace("Ö", "O");
+            a = a.Replace("Ğ", "G");
+            a = a.Replace("ı", "I");
+            a = a.Replace("ü", "U");
+            a = a.Replace("ş", "S");
+            a = a.Replace("ç", "C");
+            a = a.Replace("ğ", "G");
+            a = a.Replace("ö", "O");
+
+            return a;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen) //bağlıysa kapat
+            {
+                serialPort1.Close();
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+                label1.Text = "Status: Disconnected";
+                button1.Text = "Conntect";
+            }
+            else //bağlı değilse, bağlan
+            {
+                if (seri_port_baglan(comboBox1.Text, comboBox2.Text) == true)
+                {
+                    comboBox1.Enabled = false;
+                    comboBox2.Enabled = false;
+                    label1.Text = "Status: Connected";
+                    button1.Text = "Disconnetect";
+                }
+                else
+                {
+                    comboBox1.Enabled = true;
+                    comboBox2.Enabled = true;
+                    label1.Text = "Status: Error";
+                    button1.Text = "Conntect";
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //preamble + senkronizasyon + veri
+
+            byte[] SYNC = new byte[10] { 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0x01, 0XFE };
+            serialPort1.Write(SYNC, 0, 10);
+            seri_port_data_gonder(textBox1.Text); // + (Char)13
+
+            seri_port_data_gonder("~~~~"); // garanti olsun diye bir kaç kere tilda(~) gönderiyorum birini alamazsa birini alır
+
+            textBox2.Text += textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] portlar = SerialPort.GetPortNames();  // portları dizi halinde aldık
+
+            foreach (string port in portlar)
+            {
+                comboBox1.Items.Add(port.ToString()); // Portlarımızı combobox ın içine aldık.
+            }
+
+            comboBox2.Items.Add("300"); // BaudRate
+            comboBox2.Items.Add("150"); // BaudRate
+            comboBox2.Items.Add("110"); // BaudRate
+            comboBox2.Items.Add("75"); // BaudRate
+            comboBox2.Items.Add("600"); // BaudRate
+            comboBox2.Items.Add("1200"); // BaudRate
+            comboBox2.Items.Add("2400"); // BaudRate
+            comboBox2.Items.Add("4800"); // BaudRate
+            comboBox2.Items.Add("9600"); // BaudRate
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+
+            radioButton1.Select();
+
+            CheckForIllegalCrossThreadCalls = false;
+
+            this.MaximumSize = this.Size; //formun boyutlarini engelleyelim, kendi değerinden daha büyük olamasın
+            this.MinimumSize = this.Size; //formun boyutlarini engelleyelim, kendi değerinden daha küçük olamasın
+
+            //textBox1.Enabled = false;
+
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (veriAlmaAktifMi == true)
+            {
+                byte[] veri = new byte[1];
+                serialPort1.Read(veri, 0, 1);
+
+                if (durum == 2)
+                {
+
+                    gelenVeri[gelenSayac] = veri[0];
+                    gelenSayac++;
+                    if (gelenSayac > 30) //not: 30 karakteri geçerse zaman aşımına uğramış kabul edip, iptal ediyoruz
+                    {
+                        gelenSayac = 0;
+                        durum = 0;
+                        for (int i = 0; i < 30; i++) gelenVeri[i] = 0; //verileri sil
+                    }
+                    else if (veri[0] == '~') //enter geldiyse, veriyi onaylayalım
+                    {
+                        textBox1.Text = "";
+                        for (int i = 0; i < gelenSayac - 1; i++)
+                        {
+                            //gelenVeri[i] = 66;
+                            textBox1.Text += ((char)gelenVeri[i]).ToString();
+
+                        }
+
+
+                        textBox2.Text += textBox1.Text + "\r\n";
+                        for (int i = 0; i < 30; i++) gelenVeri[i] = 0; //verileri sil
+                        gelenSayac = 0;
+                        durum = 0;
+                    }
+
+
+                }
+                else
+                {
+
+                    //eğer program çalıştırılınca, alıcı moduna geçirilirse bu kısım çalışacak
+                    //string indata = serialPort1.ReadExisting();
+
+                    //MessageBox.Show(veri[0].ToString());
+
+                    if (durum == 0)
+                    {
+                        if (veri[0] == 0x01)
+                        {
+                            durum++;
+                        }
+                        else
+                        {
+                            durum = 0;
+                        }
+                    }
+                    else if (durum == 1)
+                    {
+                        if (veri[0] == 0xFE)
+                        {
+                            durum++;
+                        }
+                        else
+                        {
+                            durum = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void seri_port_data_gonder(string gonderilecek_veri)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Write(gonderilecek_veri);
+                    System.Threading.Thread.Sleep(150);
+                }
+                else
+                {
+                    MessageBox.Show("No connected");
+                }
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Sonuç: Başarısız !", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button3);
+            }
+
+        }
+
+        private void comboBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // comboBox1 is readonly
+            e.SuppressKeyPress = true;
+        }
+
+        private void comboBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            // comboBox2 is readonly
+            e.SuppressKeyPress = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            GorunumModeAlici("verici");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            GorunumModeAlici("alici");
+        }
+
+
+    }
+}
